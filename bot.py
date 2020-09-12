26fb7af99b828ed00767212abf91d37555a7a6ed
0muraliev <0muraliev@github.com>
2020-09-12 16:29:43 +0300

    implement OOP structure

--- bot.py
+++ bot.py
@@ -1,56 +1,56 @@
 import requests
-from bottle import (
-    run, post, response, request as bottle_request
-)
+from bottle import Bottle, response, request as bottle_request
+
 from secret_token import token
 
-BOT_URL = f'https://api.telegram.org/bot{token}/'
+
+class BotHandlerMixin:
+    BOT_URL = None
+
+    def get_chat_id(self, data):
+        """Метод извлечения chat_id из запроса telegram бота"""
+        chat_id = data['message']['chat']['id']
+        return chat_id
+
+    def get_message(self, data):
+        """Метод извлечения message_id из запроса telegram бота"""
+        message_text = data['message']['text']
+        return message_text
+
+    def send_message(self, prepared_data):
+        """Подготовленные данные должны быть в формате json, которые включают как минимум chat_id и text."""
+        message_url = self.BOT_URL + 'sendMessage'
+        requests.post(message_url, json=prepared_data)
 
 
-def get_chat_id(data):
-    """Метод извлечения chat_id из запроса telegram бота"""
-    chat_id = data['message']['chat']['id']
+class TelegramBot(BotHandlerMixin, Bottle):
+    BOT_URL = f'https://api.telegram.org/bot{token}/'
 
-    return chat_id
+    def __init__(self, *args, **kwargs):
+        super(TelegramBot, self).__init__()
+        self.route('/', callback=self.post_handler, method='POST')
 
+    def change_text_message(self, text):
+        """Метод переворота текста сообщения"""
+        return text[::-1]
 
-def get_message(data):
-    """Метод извлечения message_id из запроса telegram бота"""
-    message_text = data['message']['text']
+    def prepare_data_for_answer(self, data):
+        message = self.get_message(data)
+        answer = self.change_text_message(message)
+        chat_id = self.get_chat_id(data)
+        json_data = {
+            "chat_id": chat_id,
+            "text": answer,
+        }
+        return json_data
 
-    return message_text
-
-
-def send_message(prepared_data):
-    """Подготовленные данные должны быть в формате json, которые включают как минимум chat_id и text."""
-    message_url = BOT_URL + 'sendMessage'
-    requests.post(message_url, json=prepared_data)
-
-
-def change_text_message(text):
-    """Метод переворота текста сообщения"""
-    return text[::-1]
-
-
-def prepare_data_for_answer(data):
-    answer = change_text_message(get_message(data))
-
-    json_data = {
-        "chat_id": get_chat_id(data),
-        "text": answer,
-    }
-
-    return json_data
-
-
-@post('/')
-def main():
-    data = bottle_request.json
-
-    answer_data = prepare_data_for_answer(data)
-    send_message(answer_data)
-    return response  # status 200 OK по умолчанию
+    def post_handler(self):
+        data = bottle_request.json
+        answer_data = self.prepare_data_for_answer(data)
+        self.send_message(answer_data)
+        return response
 
 
 if __name__ == '__main__':
-    run(host='localhost', port=8080, debug=True)
+    app = TelegramBot()
+    app.run(host='localhost', port=8090)
